7d4b9b066d52c54cba8018c226e6fd46ef212146
Duncan McGregor <duncan@oneeyedmen.com>
2014-10-06 21:15:49 +0100

    Update sms.js
    Allow empty phone number

--- www/sms.js
+++ www/sms.js
@@ -15,20 +15,6 @@ smsExport.sendMessage = function(messageInfo, successCallback, errorCallback) {
            
     var phoneNumber = messageInfo.phoneNumber;
     var textMessage = messageInfo.textMessage || "Default Text from SMS plugin";
-           
-    if (! phoneNumber) {
-        console.log("Missing Phone Number");
-    
-        if (errorCallback) {
-            errorCallback({
-                code: "MISSING_PHONE_NUMBER",
-                message: "Missing Phone number"
-            });
-        }
-           
-        return;
-    }
-           
     cordova.exec(successCallback, errorCallback, "Sms", "sendMessage", [phoneNumber, textMessage]);
 };
 
