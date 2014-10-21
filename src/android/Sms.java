0c0083e366d19e0428b2f152b5ea418af7180f49
Duncan McGregor <duncan@oneeyedmen.com>
2014-10-21 12:40:37 +0100

    Always compose message on Android like iOS

--- src/android/Sms.java
+++ src/android/Sms.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.net.Uri;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -17,87 +18,67 @@ import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.telephony.SmsManager;
 
-/**
- * This class echoes a string called from JavaScript.
- */
-//Changed from CDNsms to sms
 public class Sms extends CordovaPlugin {
-    private static final String SMS_GENERAL_ERROR = "SMS_GENERAL_ERROR";
-    private static final String NO_SMS_SERVICE_AVAILABLE = "NO_SMS_SERVICE_AVAILABLE";
     private static final String SMS_FEATURE_NOT_SUPPORTED = "SMS_FEATURE_NOT_SUPPORTED";
-    private static final String SENDING_SMS_ID = "SENDING_SMS";
+    private static final String CANCELLED = "CANCELLED";
+
+    private Context context;
+    private CallbackContext callbackContext;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("sendMessage")) {
-            String phoneNumber = args.getString(0);
-            String message = args.getString(1);
-            
-            boolean isSupported = getActivity().getPackageManager().hasSystemFeature(PackageManager.FEATURE_TELEPHONY);
-            
-            if (! isSupported) {
-                JSONObject errorObject = new JSONObject();
-                
-                errorObject.put("code", SMS_FEATURE_NOT_SUPPORTED);
-                errorObject.put("message", "SMS feature is not supported on this device");
-                
-                callbackContext.sendPluginResult(new PluginResult(Status.ERROR, errorObject));
-                return false;
-            }
-            
-            this.sendSMS(phoneNumber, message, callbackContext);
-            
-            return true;
+        this.callbackContext = callbackContext;
+        this.context = cordova.getActivity().getApplicationContext();
+
+        if (!action.equals("sendMessage")) {
+            return false;
         }
-        
+        String phoneNumber = args.getString(0);
+        String message = args.getString(1);
+
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.putExtra("address", phoneNumber);
+        intent.putExtra("sms_body", message);
+        intent.setData(Uri.parse("smsto:" + phoneNumber));
+
+        if (!canHandle(intent)) {
+            return signalError(callbackContext, SMS_FEATURE_NOT_SUPPORTED, "SMS feature is not supported on this device");
+        }
+
+        cordova.startActivityForResult(this, intent, 1);
+
+        PluginResult r = new PluginResult(PluginResult.Status.NO_RESULT);
+        r.setKeepCallback(true);
+        callbackContext.sendPluginResult(r);
+        return true;
+    }
+
+    private boolean canHandle(Intent intent) {
+        return intent.resolveActivity(context.getPackageManager()) != null;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == Activity.RESULT_OK) {
+            callbackContext.success("");
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            signalError(callbackContext, "CANCELLED", "Sending was cancelled");
+        }
+    }
+
+    private boolean signalError(CallbackContext callbackContext, String code, String message) {
+        callbackContext.sendPluginResult(new PluginResult(Status.ERROR, error(code, message)));
         return false;
     }
 
-    private void sendSMS(String phoneNumber, String message, final CallbackContext callbackContext) throws JSONException {
-        PendingIntent sentPI = PendingIntent.getBroadcast(getActivity(), 0, new Intent(SENDING_SMS_ID), 0);
-
-        getActivity().registerReceiver(new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                switch (getResultCode()) {
-                case Activity.RESULT_OK:
-                    callbackContext.sendPluginResult(new PluginResult(Status.OK, "SMS message is sent successfully"));
-                    break;
-                case SmsManager.RESULT_ERROR_NO_SERVICE:
-                    try {
-                        JSONObject errorObject = new JSONObject();
-                        
-                        errorObject.put("code", NO_SMS_SERVICE_AVAILABLE);
-                        errorObject.put("message", "SMS is not sent because no service is available");
-                        
-                        callbackContext.sendPluginResult(new PluginResult(Status.ERROR, errorObject));   
-                    } catch (JSONException exception) {
-                        exception.printStackTrace();
-                    }
-                    break;
-                default:
-                    try {
-                        JSONObject errorObject = new JSONObject();
-                        
-                        errorObject.put("code", SMS_GENERAL_ERROR);
-                        errorObject.put("message", "SMS general error");
-                        
-                        callbackContext.sendPluginResult(new PluginResult(Status.ERROR, errorObject));
-                    } catch (JSONException exception) {
-                        exception.printStackTrace();
-                    }
-                    
-                    break;
-                }
-            }
-        }, new IntentFilter(SENDING_SMS_ID));
-
-        SmsManager sms = SmsManager.getDefault();
-        
-        sms.sendTextMessage(phoneNumber, null, message, sentPI, null);
-    }
-    
-    private Activity getActivity() {
-        return this.cordova.getActivity();
+    private JSONObject error(String code, String message) {
+        JSONObject errorObject = new JSONObject();
+        try {
+            errorObject.put("code", code);
+            errorObject.put("message", message);
+        } catch (JSONException e) {
+        }
+        return errorObject;
     }
 }
